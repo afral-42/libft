04e1e1d4ee3d172fe4964195bcf9df835ab1ff95
Anselme Bounouar <abounoua@z3r2p6.42lyon.fr>
2025-11-10 09:54:58 +0100

    Finished bonuses

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,29 +6,35 @@
 /*   By: abounoua <abounoua@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 17:17:07 by abounoua          #+#    #+#             */
-/*   Updated: 2025/11/07 17:26:41 by abounoua         ###   ########lyon.fr   */
+/*   Updated: 2025/11/10 09:54:39 by abounoua         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// TO DO : gerer les erreurs d'allocation avec del et free pour free ce qui a ete alloue jusqu'a maintenant
-// Checker f et del
-
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
+	t_list	*temp;
 
-	if (!lst)
+	if (!lst || !f || !del)
 		return (NULL);
 	else
 	{
 		new_list = ft_lstnew((*f)(lst->content));
+		if (!new_list)
+			return (NULL);
 		lst = lst->next;
 	}
 	while (lst != NULL)
 	{
-		ft_lstadd_back(&new_list, (*f)(lst->content));
+		temp = ft_lstnew((*f)(lst->content));
+		if (!temp)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, temp);
 		lst = lst->next;
 	}
 	return (new_list);
