b7f139b92583053b912373231b80c7f2a1ed4af0
Anselme Bounouar <abounoua@z3r3p2.42lyon.fr>
2025-11-11 08:45:20 +0100

    Corrected verification

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: abounoua <abounoua@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 16:03:50 by abounoua          #+#    #+#             */
-/*   Updated: 2025/11/06 16:13:25 by abounoua         ###   ########lyon.fr   */
+/*   Updated: 2025/11/11 08:45:02 by abounoua         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	size_t	i;
 
+	if (!s || !f)
+		return ;
 	i = 0;
 	while (s[i] != '\0')
 	{
